db594a790bf8fe240000c1d3b56a1f70d643fb41
SardorSafarov <zomin000s@gmail.com>
2023-09-04 22:53:50 +0500

    on back press done

--- app/src/main/java/com/example/apprestaran/activity/adminPanel/AdminMainActivity.kt
+++ app/src/main/java/com/example/apprestaran/activity/adminPanel/AdminMainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.Gravity
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -14,14 +15,15 @@ import com.example.apprestaran.activity.adminPanel.ui.DrawerOnOffInteface
 import com.example.apprestaran.databinding.ActivityAdminMainBinding
 import com.example.apprestaran.need.d
 import com.example.apprestaran.need.tosat
+import com.google.android.material.navigation.NavigationView
 
 
-class AdminMainActivity : AppCompatActivity(),DrawerOnOffInteface {
+class AdminMainActivity : AppCompatActivity(), DrawerOnOffInteface {
 
     private lateinit var binding: ActivityAdminMainBinding
     private lateinit var navController: NavController
     var doubleBackToExitPressedOnce = false
-    lateinit var drawer:DrawerLayout
+    lateinit var drawer: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminMainBinding.inflate(layoutInflater)
@@ -29,8 +31,40 @@ class AdminMainActivity : AppCompatActivity(),DrawerOnOffInteface {
         navController = findNavController(R.id.nav_host_fragment_activity_admin_main)
         setupWithNavController(binding.navView, navController)
         navigationFragment()
+        navigationDrawerItemClick()
         drawer = binding.drawerLayout
     }
+
+    private fun navigationDrawerItemClick() {
+        binding.navDrawerView.setNavigationItemSelectedListener(object :
+            NavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                drawer.closeDrawer(Gravity.LEFT)
+                when (item.itemId) {
+                    R.id.navigation_drawer_home -> {
+                        navController.navigate(R.id.navigation_home)
+                        true
+                    }
+
+                    R.id.navigation_drawer_worker -> {
+                        navController.navigate(R.id.navigation_worker)
+                        true
+                    }
+
+                    R.id.navigation_drawer_history -> {
+                        navController.navigate(R.id.navigation_history)
+                        true
+                    }
+
+                    else -> true
+                }
+
+                return true
+            }
+        })
+
+    }
+
 
     private fun navigationFragment() {
         binding.navView.setOnItemSelectedListener {
@@ -39,22 +73,27 @@ class AdminMainActivity : AppCompatActivity(),DrawerOnOffInteface {
                     navController.navigate(R.id.navigation_contact)
                     true
                 }
+
                 R.id.navigation_calendar -> {
                     navController.navigate(R.id.navigation_calendar)
                     true
                 }
+
                 R.id.navigation_home -> {
                     navController.navigate(R.id.navigation_home)
                     true
                 }
+
                 R.id.navigation_worker -> {
                     navController.navigate(R.id.navigation_worker)
                     true
                 }
+
                 R.id.navigation_history -> {
                     navController.navigate(R.id.navigation_history)
                     true
                 }
+
                 else -> {
                     true
                 }
@@ -63,10 +102,9 @@ class AdminMainActivity : AppCompatActivity(),DrawerOnOffInteface {
     }
 
 
-
     override fun onBackPressed() {
         if (doubleBackToExitPressedOnce) {
-            super.onBackPressed()
+            finish()
             return
         }
         doubleBackToExitPressedOnce = true
@@ -75,6 +113,7 @@ class AdminMainActivity : AppCompatActivity(),DrawerOnOffInteface {
             doubleBackToExitPressedOnce = false
         }, 2000)
     }
+
     override fun drawerOnOff() {
         if (drawer.isDrawerOpen(Gravity.LEFT)) {
             drawer.closeDrawer(Gravity.LEFT)
