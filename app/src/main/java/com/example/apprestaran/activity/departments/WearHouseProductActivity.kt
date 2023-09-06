a60f1b17be995881a9aa29b9acb7890f50bb54d6
SardorSafarov <zomin000s@gmail.com>
2023-09-06 10:51:12 +0500

    warehouse nav drawer done

--- app/src/main/java/com/example/apprestaran/activity/departments/WearHouseProductActivity.kt
+++ app/src/main/java/com/example/apprestaran/activity/departments/WearHouseProductActivity.kt
@@ -2,11 +2,36 @@ package com.example.apprestaran.activity.departments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.apprestaran.R
+import com.example.apprestaran.databinding.ActivityWearHouseBinding
 
 class WearHouseProductActivity : AppCompatActivity() {
+    lateinit var binding:ActivityWearHouseBinding
+    lateinit var drawer: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wear_house)
+        binding = ActivityWearHouseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        drawer = binding.drawerLayout
+        btnClikListener()
+    }
+
+    private fun btnClikListener() {
+        binding.btnExit.setOnClickListener {
+            finish()
+        }
+        openNavDrawer()
+    }
+
+    private fun openNavDrawer() {
+        binding.btnHamburger.setOnClickListener {
+            if (drawer.isDrawerOpen(Gravity.RIGHT)) {
+                drawer.closeDrawer(Gravity.RIGHT)
+            } else {
+                drawer.openDrawer(Gravity.RIGHT)
+            }
+        }
     }
 }
